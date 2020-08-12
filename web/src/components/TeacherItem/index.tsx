e81b4bbc5f26339e14c94511e9207293486e33c4
= <Alison Souza>
2020-08-12 02:54:54 -0300

    Web application Proffy fiished

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,52 @@ import React from 'react';
 import './style.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher{
+        id: number;
+        name: string;
+        subject: string;
+        avatar: string;
+        whatsapp: string;
+        bio: string;
+        cost: number;
+
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+    
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection(){
+        api.post("/connections", {
+            user_id: teacher.id
+        })
+    }
+
     return(
 
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars3.githubusercontent.com/u/33739228?s=460&u=0a7e01a18896312898794077ae004e07fe6e7cb0&v=4" alt="Alison Souza"/>
+                        <img src={ teacher.avatar } alt={ teacher.name }/>
                         <div>
-                            <strong>Alison Souza</strong>
-                            <span>Javascript</span>
+                            <strong>{ teacher.name }</strong>
+                            <span>{ teacher.subject }</span>
                         </div>
                     </header>
-                    <p>Entusiasta das melhores tecnologias de química avançada.
-                        <br /><br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+                    <p>{ teacher.bio }</p>
                     <footer>
                         <p>
                             Preço/hora
-                            <strong>R$ 20,00</strong>
+                            <strong>R$ { teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
                             <img src={whatsappIcon} alt="WhatsApp"/> 
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     );
